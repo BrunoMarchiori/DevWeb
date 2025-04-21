48627910c031ad78abbfa84b521e6a8e727f8308
MarchioriBruno <151454984+MarchioriBruno@users.noreply.github.com>
2025-04-21 18:39:22 -0300

    LinhasDeProdutos

--- FrontDevWeb/src/App.tsx
+++ FrontDevWeb/src/App.tsx
@@ -1,24 +1,8 @@
 import React from "react";
-import Header from "./components/Header/header";
-import Footer from "./components/Footer/footer";
-import ProductCard from "./components/ProductCard/product-card";
+import HomePage from "./pages/Home/home";
+
 const App: React.FC = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-        backgroundColor: "#989898",
-      }}
-    >
-      <Header />
-      <main style={{ flex: "1" }}>
-        <ProductCard />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <HomePage />; 
 };
 
 export default App;
--- FrontDevWeb/src/pages/Home/home.tsx
+++ FrontDevWeb/src/pages/Home/home.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  <></>;
-}
+import React from "react";
+import Header from "../../components/Header/header";
+import Footer from "../../components/Footer/footer";
+import ProductCard from "../../components/ProductCard/product-card";
+
+const HomePage: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        backgroundColor: "#989898",
+      }}
+    >
+      <Header />
+      <main style={{ flex: "1", padding: "10px" }}>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
