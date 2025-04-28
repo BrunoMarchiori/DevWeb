adf712f0fd496aba9a8e11a61834a3155adb7359
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-04-27 23:29:33 -0300

    fix: modulei o css da Home  e refatorei o código

--- FrontDevWeb/src/pages/HomePage/home.tsx
+++ FrontDevWeb/src/pages/HomePage/home.tsx
@@ -1,30 +1,20 @@
-import Header from "../../components/Header/header";
-import Footer from "../../components/Footer/footer";
 import CustomSlider from "../../components/Slider/slider";
+import styles from "./home.module.css"; // Importando o CSS do HomePage
 
 const HomePage: React.FC = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-        backgroundColor: "#989898",
-      }}
-    >
-      <Header />
-      <div></div>
-      <main style={{ flex: "1", padding: "0 50px" }}>
-        <span>Eletronicos</span>
-        <CustomSlider items={10} />
-        <span>Eletronicos</span>
-        <CustomSlider items={10} />
-        <span>Eletronicos</span>
-        <CustomSlider items={10} />
-        <span>Eletronicos</span>
-        <CustomSlider items={10} />
-      </main>
-      <Footer />
+    <div className={styles.mainDiv}>
+      
+      <div className={styles.sliderDiv}>
+          <span>Eletronicos</span>
+          <CustomSlider items={10} />
+          <span>Eletronicos</span>
+          <CustomSlider items={10} />
+          <span>Eletronicos</span>
+          <CustomSlider items={10} />
+          <span>Eletronicos</span>
+          <CustomSlider items={10} />
+        </div>
     </div>
   );
 };
