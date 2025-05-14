038471396401500291a6902e2d373a7978fda3cd
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-05-14 17:53:26 -0300

    feat: nossos nomes no trabalho

--- FrontDevWeb/src/pages/HomePage/home.tsx
+++ FrontDevWeb/src/pages/HomePage/home.tsx
@@ -4,7 +4,7 @@ import styles from "./home.module.css"; // Importando o CSS do HomePage
 const HomePage: React.FC = () => {
   return (
     <div className={styles.mainDiv}>
-      
+      <h1>Trabalho de Luiz Felipe Alves e Bruno Marchiori</h1>
       <div className={styles.sliderDiv}>
           <span>Eletronicos</span>
           <CustomSlider items={10} />
