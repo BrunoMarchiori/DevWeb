b2279e66c09b4120c1b71c1a8ea87d57fcdf799d
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-07-19 23:08:37 -0300

    Create ProdutoDTO.java

---
+++ server/src/main/java/com/bazaar/DTO/ProdutoDTO.java
@@ -0,0 +1,35 @@
+package com.bazaar.DTO;
+
+import com.bazaar.entity.ItemCarrinho;
+import com.bazaar.entity.Produto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean disponivel;
+    private Integer qtdEstoque;
+    private BigDecimal preco;
+
+
+
+
+    public ProdutoDTO(Produto produto) {
+        this.id = produto.getId();
+        this.nome = produto.getNome();
+        this.descricao = produto.getDescricao();
+        this.disponivel = produto.isDisponivel();
+        this.qtdEstoque = produto.getQtdEstoque();
+        this.preco = produto.getPreco();
+
+    }
+}
