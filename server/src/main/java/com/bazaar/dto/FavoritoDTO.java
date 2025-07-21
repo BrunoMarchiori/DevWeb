af8138473217078fa7569f657b61f7fa5305cca6
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-07-21 03:34:15 -0300

    fix: correção endpoints de favorito
    - campo de imagem estava travando a requisição

--- server/src/main/java/com/bazaar/dto/FavoritoDTO.java
+++ server/src/main/java/com/bazaar/dto/FavoritoDTO.java
@@ -13,14 +13,12 @@ import java.math.BigDecimal;
 public class FavoritoDTO {
 
     private Long id;
-    private String imagem;
     private String nome;
     private String slug;
     private BigDecimal preco;
 
     public FavoritoDTO(Produto produto) {
         this.id = produto.getId();
-        this.imagem = produto.getImagem();
         this.nome = produto.getNome();
         this.slug = produto.getSlug();
         this.preco = produto.getPreco();
--- server/src/main/java/com/bazaar/service/FavoritoService.java
+++ server/src/main/java/com/bazaar/service/FavoritoService.java
@@ -30,13 +30,20 @@ public class FavoritoService {
     @Autowired
     private ProdutoRepository productRepository;
 
+    @Transactional()
     public void addFavorite(Long userId, Long productId) {
-        Usuario user = userRepository.findById(userId).orElseThrow();
-        Produto product = productRepository.findById(productId).orElseThrow();
-        user.getFavoriteProducts().add(product);
-        userRepository.save(user);
+        try{
+            Usuario user = userRepository.findById(userId).orElseThrow();
+            Produto product = productRepository.findById(productId).orElseThrow();
+            user.getFavoriteProducts().add(product);
+            userRepository.save(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
+    @Transactional()
     public void removeFavorite(Long userId, Long productId) {
         Usuario user = userRepository.findById(userId).orElseThrow();
         Produto product = productRepository.findById(productId).orElseThrow();
@@ -44,13 +51,16 @@ public class FavoritoService {
         userRepository.save(user);
     }
 
+    @Transactional(readOnly = true)
     public Set<FavoritoDTO> getFavorites(Long userId) {
         Usuario user = userRepository.findById(userId).orElseThrow();
 
-        return user.getFavoriteProducts()
+        Set<FavoritoDTO> a = user.getFavoriteProducts()
                 .stream()
                 .map(FavoritoDTO::new)
                 .collect(Collectors.toSet());
+
+        return a;
 
 
     }
